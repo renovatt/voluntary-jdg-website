cbce91ea78e5095d3f9da1a934492ef030736df5
will <wlymes@gmail.com>
2023-08-09 15:07:28 -0300

    style: style base config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          950: '#002550',
+          800: '#025A9A',
+          400: '#00EBF0',
+          500: '#18cba2',
+          DEFAULT: '#00EBF0',
+        },
+        secondary: {
+          950: '#0A0A0A',
+          900: '#171717',
+          800: '#262626',
+          500: '#7D7A7A',
+          50: '#FAFAFA',
+          DEFAULT: '#FAFAFA',
+        },
+      },
     },
   },
   plugins: [],
