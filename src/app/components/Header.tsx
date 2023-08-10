7e603daeaae7e0266076c1bc8b146490139f63d4
will <wlymes@gmail.com>
2023-08-10 18:38:10 -0300

    feat: header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import Navbar from './NavBar'
+import logo from '@/assets/jdg.png'
+import NavbarMobile from './NavbarMobile'
+import { useToggle } from '@/hooks/useToggle'
+
+const Header = () => {
+  const { modal, openModal, closeModal } = useToggle()
+
+  return (
+    <header className="fixed left-0 top-0 flex h-20 w-full items-center justify-between bg-primary px-16 transition-all">
+      <Link href="/" className="flex">
+        <figure className="flex h-10 w-10 items-center justify-center rounded-md bg-white">
+          <Image src={logo} alt="logo" className="h-full w-full object-cover" />
+        </figure>
+      </Link>
+
+      <Navbar openModal={openModal} />
+      <NavbarMobile modal={modal} closeModal={closeModal} />
+
+      <Link
+        href="#contact"
+        className="hidden h-10 w-36 items-center justify-center rounded-lg bg-primary-400 text-base font-extrabold text-secondary-950 transition-all hover:text-white md:flex"
+      >
+        Contato
+      </Link>
+    </header>
+  )
+}
+
+export default Header
