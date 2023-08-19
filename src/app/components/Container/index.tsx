9e764f2a12c0c37efab1dcced0651bc670642442
will <wlymes@gmail.com>
2023-08-19 11:58:32 -0300

    feat: container updated

--- src/app/components/Container/index.tsx
+++ src/app/components/Container/index.tsx
@@ -1,8 +1,8 @@
-import { ContainerTypeProps } from '@/@types'
+import { ContainerTypeProps } from './types'
 
 const Container = ({ children }: ContainerTypeProps) => {
   return (
-    <main className="flex min-h-screen flex-1 flex-col items-center justify-start">
+    <main className="flex min-h-screen flex-1 flex-col items-center justify-center">
       {children}
     </main>
   )
