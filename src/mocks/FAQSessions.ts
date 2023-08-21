b5b6ad8873cbffe7ed0ed5e555da87d069691f75
will <wlymes@gmail.com>
2023-08-21 08:15:30 -0300

    feat: faq sessions mock

---
+++ src/mocks/FAQSessions.ts
@@ -0,0 +1,39 @@
+export const FAQSessionsMock = [
+  {
+    id: 1,
+    question: 'Como posso me inscrever?',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus minima ex sit fugiat. Sit aliquam minima ad similique quae obcaecati sunt, delectus eos excepturi mollitia ducimus ex at quam laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 2,
+    question: 'Quais tecnologias utilizamos?',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus minima ex sit fugiat. Sit aliquam minima ad similique quae obcaecati sunt, delectus eos excepturi mollitia ducimus ex at quam laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 3,
+    question: 'Qual é a frequência das reuniões ou encontros do grupo?',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus minima ex sit fugiat. Sit aliquam minima ad similique quae obcaecati sunt, delectus eos excepturi mollitia ducimus ex at quam laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 4,
+    question:
+      'É necessário ter experiência prévia em programação para participar?',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus minima ex sit fugiat. Sit aliquam minima ad similique quae obcaecati sunt, delectus eos excepturi mollitia ducimus ex at quam laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 5,
+    question: 'Quais projetos vocês estão trabalhando atualmente?',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus minima ex sit fugiat. Sit aliquam minima ad similique quae obcaecati sunt, delectus eos excepturi mollitia ducimus ex at quam laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 6,
+    question: 'Vocês aceitam membros de outras cidades?',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus minima ex sit fugiat. Sit aliquam minima ad similique quae obcaecati sunt, delectus eos excepturi mollitia ducimus ex at quam laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+]
