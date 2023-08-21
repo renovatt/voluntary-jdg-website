498f9e367aa59280717a03b23a6b363bda3bdf5b
will <wlymes@gmail.com>
2023-08-21 08:31:27 -0300

    style: faq animate added

--- src/app/components/FaqSessionContent/index.tsx
+++ src/app/components/FaqSessionContent/index.tsx
@@ -1,7 +1,7 @@
 import { FaqSessionContentProps } from './types'
 
 const FaqSessionContent = ({ content }: FaqSessionContentProps) => (
-  <p className="mb-6 mt-4 animate-faqFade px-4 text-base leading-8 text-secondary transition-all">
+  <p className="mb-6 mt-4 animate-faq px-4 text-base leading-8 text-secondary transition-all">
     {content}
   </p>
 )
