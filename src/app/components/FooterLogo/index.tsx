0dcd47f085ac04d5e9d068d0710b37b544b9a580
will <wlymes@gmail.com>
2023-08-24 00:19:21 -0300

    feat: footer logo

---
+++ src/app/components/FooterLogo/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterLogo = () => {
+  return (
+    <section className="flex h-28 w-36 flex-col items-start justify-between">
+      <h1 className="text-5xl font-bold text-primary">jdg.</h1>
+      <p className="flex flex-col text-xs text-secondary">
+        Junte-se à revolução
+        <span>da programação! 🚀</span>
+      </p>
+    </section>
+  )
+}
+
+export default FooterLogo
