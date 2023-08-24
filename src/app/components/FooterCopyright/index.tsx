4903091175db9d7c61ee6dbc8e3e113c381291a4
will <wlymes@gmail.com>
2023-08-24 00:18:40 -0300

    feat: footer copyright

---
+++ src/app/components/FooterCopyright/index.tsx
@@ -0,0 +1,14 @@
+const FooterCopyright = () => {
+  const date = new Date()
+  const currentYear = date.getFullYear()
+
+  return (
+    <section className="mb-20 mt-8 flex items-center justify-center">
+      <h6 className="px-4 text-center text-xs text-secondary">
+        © Juniors Developers Group {currentYear}. Todos os direitos reservados.
+      </h6>
+    </section>
+  )
+}
+
+export default FooterCopyright
