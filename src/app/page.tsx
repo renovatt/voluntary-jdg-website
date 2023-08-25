ae12c2145fa00c5a0ccb94b2223aa66e2cc63db3
will <wlymes@gmail.com>
2023-08-25 11:29:28 -0300

    feat: projects page implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Banner from './components/Banner'
 import Faq from './components/Faq'
+import Projects from './components/Projetcts'
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Projects />
       <Faq />
     </>
   )
