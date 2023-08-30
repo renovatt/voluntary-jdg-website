baaf0fafa4d4dfb88fa719110a2ec29c13cff447
matheusOliv23 <matheushenri1996@gmail.com>
2023-08-30 19:18:25 -0300

    refactor: refactoring DepositionItem

--- src/app/components/Depositions/DepositionsItem.tsx
+++ src/app/components/Depositions/DepositionsItem.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { DepositionsItemProps } from './types'
 
-export default function DepositionsItem({descrition,imgUrl, name}: DepositionsItemProps) {
+export default function DepositionsItem({description,imgUrl,name, id}: DepositionsItemProps) {
   return (
-    <div className='flex flex-col text-zinc-50 sm:flex-row items-center justify-between gap-6 md:gap-10'>
+    <div className='flex flex-col text-zinc-50 sm:flex-row items-start justify-between gap-6 md:gap-10'>      
       <figure className='space-y-6'>
-        <p className='lg:max-w-[324px] leading-8 xl:max-w-[488px] w-full'>{descrition}</p> 
+        <img src={'/icons/quotation.svg'} alt="Quotation" className='mb-14' />  
+        <p className='lg:max-w-[324px] leading-8 xl:max-w-[488px] w-full'>{description}</p> 
         <span className='font-bold hidden sm:block'>{name}</span>
-      </figure>
-     
-      <img src={imgUrl} className='border-2 border-primary rounded-lg' alt={name} />
+      </figure>     
+      <img src={imgUrl} className='border-2 border-primary rounded-lg' alt={`${id}${name}`} />
       <span className='font-bold sm:hidden'>{name}</span>
     </div>
   )
