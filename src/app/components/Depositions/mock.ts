1dc388617da313ca662c30ac8d13ebc7d162103b
matheusOliv23 <matheushenri1996@gmail.com>
2023-08-30 19:52:28 -0300

    fix: fixing layout

--- src/app/components/Depositions/mock.ts
+++ src/app/components/Depositions/mock.ts
@@ -7,13 +7,13 @@ export const sliderItemsMock = [
 },
 {
   id: 2,
-  image: '/people.png',
+  image: '/people2.jpg',
   name: "Carlos Souza",
   text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
   },
 {
   id: 3,
-  image: '/people.png',
+  image: '/people2.jpg',
   name: "Julio Fernandes",
   text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
 }
