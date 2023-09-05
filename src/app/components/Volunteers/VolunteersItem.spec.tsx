0be98d95b4b925e714d50846bd7f08a65daa4c44
matheusOliv23 <matheushenri1996@gmail.com>
2023-09-05 19:47:33 -0300

    feat: add VolunteersItem component

---
+++ src/app/components/Volunteers/VolunteersItem.spec.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react"
+import VolunteersItem from "./VolunteersItem"
+
+
+const data = {
+  image: '/image.png',
+  name: 'Carlos',
+  jobPosition: 'Dev Front-end'
+}
+
+const {getByAltText,getByText} = screen
+
+describe('VolunteersItem', () => {
+  it('should render component by default', () => {
+    render(<VolunteersItem  {...data} />)
+
+    const image = getByAltText(data.name)
+    const name = getByText(data.name)
+    const jobPosition = getByText(data.jobPosition)
+
+    expect(image).toBeInTheDocument()
+    expect(name).toBeInTheDocument()
+    expect(jobPosition).toBeInTheDocument()
+  })
+})
