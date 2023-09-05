e253cd723bde9f53c336777c59b846f00f10d5a2
matheusOliv23 <matheushenri1996@gmail.com>
2023-09-05 19:47:41 -0300

    feat: add Volunteers section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Banner from './components/Banner'
 import Depositions from './components/Depositions'
 import Faq from './components/Faq'
 import Projects from './components/Projetcts'
+import Volunteers from './components/Volunteers'
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Volunteers />
       <Projects />
       <Depositions />
       <Faq />
