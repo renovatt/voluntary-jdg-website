eaa7511561984501ac2f30f59e7d99fa1bc594f9
matheusOliv23 <matheushenri1996@gmail.com>
2023-09-05 19:28:12 -0300

    feat: add mocks

---
+++ src/app/components/Volunteers/mock.ts
@@ -0,0 +1,164 @@
+export const volunteersMock = [
+  {
+    id: 1,
+    image: "/05.png",
+    name: "João da Silva",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 2,
+    image: "/02.png",
+    name: "Maria Santos",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 3,
+    image: "/01.png",
+    name: "Pedro Oliveira",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 4,
+    image: "/03.png",
+    name: "Ana Pereira",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 5,
+    image: "/01.png",
+    name: "Carlos Rodrigues",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 6,
+    image: "/02.png",
+    name: "Isabel Costa",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 7,
+    image: "/06.png",
+    name: "Rafael Fernandes",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 8,
+    image: "/02.png",
+    name: "Luisa Alves",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 9,
+    image: "/04.png",
+    name: "Miranda Cardoso",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 10,
+    image: "/woman.png",
+    name: "Mariana Sousa",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 11,
+    image: "/01.png",
+    name: "Fernando Santos",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 12,
+    image: "/02.png",
+    name: "Sofia Oliveira",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 13,
+    image: "/05.png",
+    name: "Daniel Pereira",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 14,
+    image: "/woman.png",
+    name: "Amanda Rodrigues",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 15,
+    image: "/04.png",
+    name: "Daniela Costa",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 16,
+    image: "/02.png",
+    name: "Camila Fernandes",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 17,
+    image: "/05.png",
+    name: "Ricardo Alves",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 18,
+    image: "/woman.png",
+    name: "Juliana Cardoso",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 19,
+    image: "/06.png",
+    name: "Paulo Sousa",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 20,
+    image: "/02.png",
+    name: "Patricia Santos",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 21,
+    image: "/05.png",
+    name: "Marcos Oliveira",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 22,
+    image: "/02.png",
+    name: "Eduarda Pereira",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 23,
+    image: "/04.png",
+    name: "Luiza Rodrigues",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 24,
+    image: "/woman.png",
+    name: "Carolina Costa",
+    job_position: "Dev Mobile"
+  },
+  {
+    id: 25,
+    image: "/01.png",
+    name: "Fábio Fernandes",
+    job_position: "Dev Front-end"
+  },
+  {
+    id: 26,
+    image: "/02.png",
+    name: "Renata Alves",
+    job_position: "Dev Back-end"
+  },
+  {
+    id: 27,
+    image: "/06.png",
+    name: "Diego Cardoso",
+    job_position: "Dev Mobile"
+  }
+]
