4a539b91d021b1c47c9ff5f2ec330dc4784bec5a
will <wlymes@gmail.com>
2023-09-08 18:49:21 -0300

    feat: route and page vacancie created

---
+++ src/app/components/VacanciesPage/index.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { useState } from 'react'
+import { Vacancies } from '@/mocks/Vacancies'
+import CardVacancies from '../CardVacancies'
+import VacanciesHeader from '../VacanciesHeader'
+import VacancieDropdown from '../VacancieDropdown'
+import { VacancieInputFilter } from '../VacancieInputFilter'
+
+const VacanciesPage = () => {
+  const [searchText, setSearchText] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('')
+
+  const filteredVacancies = Vacancies.filter((vacancie) => {
+    const categoryMatch =
+      !selectedCategory || vacancie.category === selectedCategory
+    const textMatch =
+      !searchText ||
+      vacancie.title.toLowerCase().includes(searchText.toLowerCase())
+
+    return categoryMatch && textMatch
+  })
+
+  const handleCategorySelect = (categoryName: string) => {
+    setSelectedCategory(categoryName)
+    setSearchText('')
+  }
+
+  const handleInputFilter = (input: string) => {
+    setSearchText(input)
+    setSelectedCategory('')
+  }
+
+  return (
+    <section className="container flex flex-col items-center justify-center px-4 md:px-20">
+      <VacanciesHeader />
+      <section className="mb-10 mt-20 flex w-full flex-col items-start justify-between gap-4 md:flex-row md:items-center">
+        <VacancieInputFilter
+          value={searchText}
+          onChange={(e) => handleInputFilter(e.target.value)}
+        />
+        <VacancieDropdown handleCategorySelect={handleCategorySelect} />
+      </section>
+
+      <section className="flex w-full flex-col items-center justify-center md:flex-row md:flex-wrap md:justify-between">
+        {filteredVacancies &&
+          filteredVacancies.map((vacancie) => (
+            <CardVacancies key={vacancie.id} {...vacancie} />
+          ))}
+      </section>
+    </section>
+  )
+}
+
+export default VacanciesPage
