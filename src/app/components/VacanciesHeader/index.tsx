846a9a3d73dff31de027ec841647a522954a7c30
will <wlymes@gmail.com>
2023-09-08 18:45:21 -0300

    feat: vacancies header created

---
+++ src/app/components/VacanciesHeader/index.tsx
@@ -0,0 +1,15 @@
+const VacanciesHeader = () => {
+  return (
+    <header className="mt-10 flex flex-col items-center justify-center">
+      <h1 className="my-4 flex flex-col items-center justify-center text-xl font-bold text-primary-400 md:text-3xl">
+        Encontre sua vaga <span>voluntária aqui!</span>
+      </h1>
+      <h2 className="flex flex-col items-center justify-center text-xs text-white md:text-base">
+        Faça parte do Juniors Developers Group:{' '}
+        <span>onde o talento é voluntário e a inovação é infinita!</span>
+      </h2>
+    </header>
+  )
+}
+
+export default VacanciesHeader
