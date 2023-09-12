0b1c5a69b3df87aa0c66c05955c8d6f11da6d028
Carlos Davi <carlosdavi004@gmail.com>
2023-09-11 23:49:53 -0300

    Feat: Criado estrutura inicial - VacanciesDetailsPage | Componentes

---
+++ src/app/components/VacanciesDetailsPage/index.tsx
@@ -0,0 +1,11 @@
+const VacanciesDetailsPage = () => {
+  return (
+    <header
+      className="mt-10 flex flex-col items-center justify-center"
+    >
+      <h1>TESTANDO</h1>
+    </header>
+  )
+}
+
+export default VacanciesDetailsPage
