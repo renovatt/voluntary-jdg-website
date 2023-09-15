ebb099e674938598807c85e2dc42d2d3413f6590
will <wlymes@gmail.com>
2023-09-15 13:40:31 -0300

    feat: card post created

---
+++ src/app/components/CardPost/index.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import { CardPostProps } from './types'
+
+const CardPost = ({
+  title,
+  subtitle,
+  description,
+  image,
+  createdAt,
+}: CardPostProps) => {
+  return (
+    <section className="container flex flex-col items-center justify-center gap-4 px-4 py-10 md:px-0">
+      <h1 className="max-w-2xl text-center text-3xl font-semibold text-primary md:text-4xl">
+        {title}
+      </h1>
+      <span className="text-xs text-secondary-500">{createdAt}</span>
+
+      <article className="">
+        <figure className="h-96 w-full">
+          <Image
+            src={image}
+            width={1000}
+            height={1000}
+            className="h-full w-full rounded-t-lg object-cover"
+            alt="post-image"
+          />
+        </figure>
+
+        <h2 className="mt-16 text-2xl font-bold text-secondary">{subtitle}</h2>
+        <p className="mt-4 text-base leading-9 text-secondary-500">
+          {description}
+        </p>
+      </article>
+    </section>
+  )
+}
+export default CardPost
