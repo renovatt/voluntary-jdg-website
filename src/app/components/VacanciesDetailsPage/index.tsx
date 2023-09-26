dd9b4d6eb1309cbbda7e8aee8cc768a21d28fc6a
Carlos Davi <carlosdavi004@gmail.com>
2023-09-26 11:07:05 -0300

    Feat:Atualizado responsividade com tailwind

--- src/app/components/VacanciesDetailsPage/index.tsx
+++ src/app/components/VacanciesDetailsPage/index.tsx
@@ -19,27 +19,27 @@ const VacanciesDetailsPage = () => {
           <Image src={arrowBack} alt="Seta para voltar" className="transform rotate-180 cursor-pointer transition-all duration-300 hover:opacity-70"/>
         </div>
 
-        <div className="border border-[#00EBF0] flex flex-col bg-[#191D20] rounded-[8px] p-[32px]">
-            <div className="bg-[#23282d] w-[245px] px-[16px] py-[10px] text-center rounded-[8px]  mb-[8px] ml-[32px]">
-              <h3 className="text-[#FAFAFA] text-[14px]">Estamos procurando um novo</h3>
+        <div className="border border-[#00EBF0] flex flex-col bg-[#191D20] rounded-sm p-8">
+            <div className="bg-[#23282d] w-100 md:w-[300px] px-2 py-2 text-center rounded-sm mb-5">
+              <h3 className="text-[#FAFAFA] text-base">Estamos procurando um novo</h3>
             </div>
 
-            <div className="flex flex-col md:flex-row mt-[8px] ml-[32px] justify-between text-[#FAFAFA]">
-              <h1 className="font-semibold text-[24px] mb-4 md:mb-0">Desenvolvedor front-end</h1>  
-              <span className="font-semibold text-[18px] md:mr-10">Incrições até 20/08/2023</span>
+            <div className="flex flex-col md:flex-row mb-5 justify-between text-[#FAFAFA]">
+              <h1 className="font-semibold text-2xl mb-4 md:mb-0 ">Desenvolvedor front-end</h1>  
+              <span className="font-semibold text-base">Incrições até 20/08/2023</span>
             </div>
 
-            <div className="mt-[8px] mb-[24px] ml-[32px] text-[#00EBF0] w-[50%] flex">
-              {['HTML', 'CSS', 'Javascript', 'React'].map((item, index, array) => (
-                <span className="flex items-center" key={index}>
-                <span className="text-[16px]">{item}</span>
+            <div className="flex flex-wrap mb-5 text-[#00EBF0] w-100">
+              {['HTML', 'CSS', 'Javascript', 'React','Java'].map((item, index, array) => (
+                <span key={index}>
+                <span className="text-base">{item}</span>
                 {index !== array.length - 1 && <span className="ml-2 mr-2">•</span>}
                 </span>
               ))}
             </div>
 
-            <div  className=" mb-[32px] ml-[32px] mr-[32px] text-[#fafafa]">
-              <p className="text-[16px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea modi porro quaerat magni. Recusandae placeat eaque, 
+            <div  className=" text-[#fafafa] mb-5">
+              <p className="text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea modi porro quaerat magni. Recusandae placeat eaque, 
               veniam doloribus, quidem perferendis error culpa distinctio fugiat earum, tenetur laborum laboriosam beatae odit.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea modi porro quaerat magni. Recusandae placeat eaque, 
               veniam doloribus, quidem perferendis error culpa distinctio fugiat earum, tenetur laborum laboriosam beatae odit.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea modi porro quaerat magni. Recusandae placeat eaque, 
               veniam doloribus, quidem perferendis error culpa distinctio fugiat earum, tenetur laborum laboriosam beatae odit.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea modi porro quaerat magni. Recusandae placeat eaque, 
@@ -50,52 +50,52 @@ const VacanciesDetailsPage = () => {
         </div>
       </section>
       
-      <section className="w-[90%] mt-[88px] pt-[64px] pr-[42px] pb-[64px] pl-[42px] flex flex-col bg-[#272727] rounded-[8px]">
-        <div className=" w-[95%] mx-auto text-[#FAFAFA] mt-4 mb-4 ml-5 mb-[72px]">
-          <h1 className=" text-[32px]  pb-5 border-b border-[rgba(250,250,250,0.7)] ">Voluntariar-se</h1>
+      <section className="w-[90%] mt-20 pt-14 pb-14 flex flex-col bg-[#272727] rounded-sm">
+        <div className=" w-[90%] mx-auto text-[#FAFAFA] mt-5 mb-16">
+          <h1 className=" text-3xl pb-5 border-b border-[rgba(250,250,250,0.7)] ">Voluntariar-se</h1>
         </div>
 
-        <form className="w-[95%] mx-auto mt-4 mb-4 ml-5">
-          <fieldset className="">
-            <div className="flex flex-col mb-[50px]">
-                <label className="text-[#FAFAFA] block mb-[16px]" htmlFor="">Nome completo<span className="text-[#00EBF0]">*</span></label>
-                <div className="bg-[#1A1A1A] rounded-[8px] py-[10px] flex justify-around">
-                  <Image src={userIcon} alt="Icone de usuario" className="block w-[24px] ml-[32px]"/>
-                  <input type="text" className="bg-transparent text-[#FAFAFA] h-[32px] ml-[24px] pr-[20px] focus:outline-none" style={{ width: 'calc(100% - 100px)' }} />
+        <form className="w-[90%] mx-auto mb-5">
+          <fieldset className="flex flex-col justify-around">
+            <div className="flex flex-col mb-10">
+                <label className="text-[#FAFAFA] block mb-4" htmlFor="">Nome completo<span className="text-[#00EBF0] text-base">*</span></label>
+                <div className="bg-[#1A1A1A] rounded-sm py-3 flex justify-around">
+                  <Image src={userIcon} alt="Icone de usuario" className="block w-[24px] ml-4"/>
+                  <input type="text" className="bg-transparent text-[#FAFAFA] h-[32px] pr-5 focus:outline-none" style={{ width: 'calc(100% - 100px)' }} />
                 </div>
             </div>
             
-            <div className="flex flex-col mb-[50px]">
-              <label className="text-[#FAFAFA] block mb-[16px]" htmlFor="">E-mail<span className="text-[#00EBF0]">*</span></label>
-              <div className="bg-[#1A1A1A] rounded-[8px] py-[10px] flex justify-around">
-                <Image src={emailIcon} alt="Icone de e-mail" className="block w-[24px] ml-[32px]" />
-                <input type="email" className="bg-transparent text-[#FAFAFA] focus:outline-none  h-[32px] ml-[24px] pr-[20px]" style={{ width: 'calc(100% - 70px)' }} />
+            <div className="flex flex-col mb-10">
+              <label className="text-[#FAFAFA] block mb-4" htmlFor="">E-mail<span className="text-[#00EBF0] text-base">*</span></label>
+              <div className="bg-[#1A1A1A] rounded-sm py-3 flex justify-around">
+                <Image src={emailIcon} alt="Icone de e-mail" className="block w-[24px] ml-4" />
+                <input type="email" className="bg-transparent text-[#FAFAFA] focus:outline-none  h-[32px] pr-5" style={{ width: 'calc(100% - 100px)' }} />
               </div>
             </div>
             
-            <div className="flex flex-col mb-[50px]">
-              <label htmlFor="" className="text-[#FAFAFA] block mb-[16px]">Telefone<span className="text-[#00EBF0]">*</span></label>
-              <div className="bg-[#1A1A1A] rounded-[8px] py-[10px] flex justify-around">
-                <Image src={phoneIcon} alt="Icone de telefone" className="block w-[24px] ml-[32px]"/>
-                <input type="number" className="bg-transparent text-[#FAFAFA] h-[32px] focus:outline-none ml-[24px] pr-[20px]" style={{ width: 'calc(100% - 70px)' }}/>
+            <div className="flex flex-col mb-10">
+              <label htmlFor="" className="text-[#FAFAFA] block mb-4">Telefone<span className="text-[#00EBF0] text-base">*</span></label>
+              <div className="bg-[#1A1A1A] rounded-sm py-3 flex justify-around">
+                <Image src={phoneIcon} alt="Icone de telefone" className="block w-[24px] ml-4"/>
+                <input type="number" className="bg-transparent text-[#FAFAFA] h-[32px] focus:outline-none  pr-5" style={{ width: 'calc(100% - 100px)' }}/>
               </div>
             </div>
 
-            <div className="flex flex-col md:flex-row justify-between mb-[50px]">
+            <div className="flex flex-col md:flex-row justify-between mb-10">
               
-              <div className="flex flex-col mt-5 w-full md:w-[48%] mb-5 md:mb-0">
-                <label htmlFor="" className="text-[#FAFAFA] block mb-[16px]">Linkedln<span className="text-[#00EBF0]">*</span></label>
-                <div className="bg-[#1A1A1A] rounded-[8px] py-[10px] flex justify-around">
-                  <Image src={linkedlnIcon} alt="Icone de Linkedln" className="block w-[24px] ml-[32px]" />
-                  <input type="text" className="bg-transparent text-[#FAFAFA] h-[32px] focus:outline-none ml-[24px] pr-[20px] w-full" style={{ width: 'calc(100% - 70px)' }} />
+              <div className="flex flex-col w-100 md:w-[48%] mb-5 md:mb-0 ">
+                <label htmlFor="" className="text-[#FAFAFA] block mb-4">Linkedln<span className="text-[#00EBF0] text-base">*</span></label>
+                <div className="bg-[#1A1A1A] rounded-sm py-3 flex justify-around">
+                  <Image src={linkedlnIcon} alt="Icone de Linkedln" className="block w-[24px] ml-4" />
+                  <input type="text" className="bg-transparent text-[#FAFAFA] h-[32px] focus:outline-none pr-5" style={{ width: 'calc(100% - 100px)' }} />
                 </div>
               </div>
                           
-              <div className="flex flex-col mt-5 w-full md:w-[48%]">
-                <label htmlFor="" className="text-[#FAFAFA] block mb-[16px]">Github</label>
-                <div className="bg-[#1A1A1A] rounded-[8px] py-[10px] flex justify-around">
-                  <Image src={githubIcon} alt="Icone de Github" className="block w-[24px] ml-[32px]"/>
-                  <input type="text" className="bg-transparent text-[#FAFAFA] h-[32px] focus:outline-none ml-[24px] pr-[20px] w-full" style={{ width: 'calc(100% - 70px)' }} />
+              <div className="flex flex-col w-100 md:w-[48%] mb-5 md:mb-0 ">
+                <label htmlFor="" className="text-[#FAFAFA] block mb-4">Github</label>
+                <div className="bg-[#1A1A1A] rounded-sm py-3 flex justify-around">
+                  <Image src={githubIcon} alt="Icone de Github" className="block w-[24px] ml-4"/>
+                  <input type="text" className="bg-transparent text-[#FAFAFA] h-[32px] focus:outline-none pr-5" style={{ width: 'calc(100% - 100px)' }} />
                 </div>
               </div>
             
@@ -113,7 +113,7 @@ const VacanciesDetailsPage = () => {
                 </div>
                 <span className="text-[#fafafa] mt-[20px] mb-[40px] ">Formato: PDF</span>
               </div>
-              <button className="bg-[rgba(0,235,240,0.2)] border border-[#00EBF0] w-[100%] pt-3 pb-3 mt-[110px] text-[#fafafa] rounded-[8px] cursor-pointer transition-all 
+              <button className="bg-[rgba(0,235,240,0.2)] border border-[#00EBF0] w-[100%] pt-3 pb-3 mt-[110px] text-[#fafafa] rounded-sm cursor-pointer transition-all 
               duration-300 hover:opacity-80 ">Enviar</button>
             </div>
           </fieldset>
